5dd2e7ba90ad364c3a94fdb472047cfda2a38ba6
jopraneeth <116384787+jopraneeth@users.noreply.github.com>
2024-06-28 22:55:02 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,154 @@
+
+
+let allMusic = [
+  {
+    name: "A Thousand Years",
+    artist: "Christina Perri",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ae Dil Hai Mushkil",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "All We Know",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Allasani Vaari",
+    artist: "Shreya Ghoshal",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Arcade",
+    artist: "Duncan Laurence",
+    img: "music-6",
+    src: "music-6"
+  },
+
+  {
+    name: "Closer",
+    artist: "Chainsmokers",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Darshana",
+    artist: "Darshana Rajendran",
+    img: "music-9",
+    src: "music-9"
+  }, 
+  {
+    name: "Enta Chithram",
+    artist: "Anurag Kulkarni",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Everything I Wanted",
+    artist: "Billie Eilish",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Heer Ranjha",
+    artist: "Bhuvan Bam",
+    img: "music-11",
+    src: "music-11"
+  }, 
+  {
+    name: "Liggi",
+    artist: "Ritviz",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Love Me Like You Do",
+    artist: "Ellie Goulding",
+    img: "music-13",
+    src: "music-13"
+  }, 
+  {
+    name: "Lovely",
+    artist: "Billie Eilish",
+    img: "music-14",
+    src: "music-14"
+  }, 
+  {
+    name: "Mehabooba",
+    artist: "Ananya Bhat",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Ninnila",
+    artist: "Armaan Malik",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Ocean Eyes",
+    artist: "Billie Eilish",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Oh Isha",
+    artist: "Armaan Malik",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Ramuloo Ramulaa",
+    artist: "Anurag Kulkarni",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Rockabye",
+    artist: "Clean Bandit",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Sage",
+    artist: "Ritviz",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "See You Again",
+    artist: "Wiz Khalifa",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "The Nights",
+    artist: "Avicii",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Tholi Prema",
+    artist: "Kala bhairava",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "We Don't Talk Anymore ",
+    artist: "Charlie Puth",
+    img: "music-24",
+    src: "music-24"
+  },{
+    name: "Without Me",
+    artist: "Halsey",
+    img: "music-25",
+    src: "music-25"
+  },
+];
